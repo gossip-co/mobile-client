4bda602ecad43722ffd66b5fcf58dd56d4799051
aditya-pushkar <adityapushkar850@gmail.com>
2023-02-09 05:18:29 +0530

    Account Component added

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,18 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
 
 import Notifications from '../components/settings/Notifications'
 import PaymentMethods from '../components/settings/PaymentMethods'
 import ExtraOptions from '../components/settings/ExtraOptions'
 import Logout from '../components/settings/Logout'
-import { ScrollView } from 'react-native-gesture-handler'
+import Account from '../components/settings/Account'
 
+
+
+// Improve the UI, not working properly , put navigation into view and produc the error,
+// After some point/height the scrollview is not working 
 const Settings = () => {
+  const isUserHasGroup = true
   return (
-    <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+<ScrollView  bounces={false} showsVerticalScrollIndicator={false} >
     <View className="h-screen bg-white items-center pt-0.5 px-3" >
 
-      <Notifications/>
+        <Notifications/>
       
       <View className="my-5">
       <PaymentMethods/>
@@ -23,10 +29,17 @@ const Settings = () => {
       </View>
 
       <View className="my-5">
+      {isUserHasGroup && <Account/>}
+      </View>
+
+      <View className="">
         <Logout/>
       </View>
+
+      
     </View>
     </ScrollView>
+
   )
 }
 
